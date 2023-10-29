037e8880422f96d36d03cea62c38aea4406c633c
ConstantinM <constantinminea@outlook.com>
2023-10-29 17:05:37 +0000

    modified astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+// https://thelinuxsystem
+export default defineConfig({
+   site: 'https://thelinuxsystem',
+   base: '/thelinuxsystem',
+});
